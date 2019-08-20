ad033b0286443e817233ff739fd04c5e938e0f4e
bngr <45574053+bngr@users.noreply.github.com>
2019-08-20 09:33:29 -0500

    Add files via upload
    Takes list of IP's and uses qwInsta to view current sessions. Gives ability to logoff remote session.

---
+++ MassServerLogoff.ps1
@@ -0,0 +1,33 @@
+﻿param (
+    [parameter(Position=0)]
+    [String]
+    $File=$(throw "Usage: './MassServerLogoff.ps1 -File [filename]'")
+)
+
+$Global:serverlist = Get-Content $File
+
+function loop {
+    foreach ($Global:server in $serverlist){
+        Main
+    }
+}
+
+function qwServer {
+    Write-Host "Current sessions for $server" -ForegroundColor white -BackgroundColor red
+    qwinsta /SERVER:$server
+}
+
+function qwLogoff {
+    $session = Read-Host "Session"
+        if ($session -like "*x*"){
+            Continue
+        }
+    logoff $session /SERVER:$server /V
+}
+
+function Main {
+     qwServer
+     qwLogoff
+}
+
+loop
